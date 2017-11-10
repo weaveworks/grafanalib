582840021851c42322b878720c6ca391090489a6
Fabian Holler <mail@fholler.de>
2017-11-10 17:26:24 +0100

    templating: make setting label optional (#92)
    grafanalib required that a value for the label attribute of a Template
    is set.
    The grafana GUI does not require to set a label. If none is set, the
    attributes value is null and the Name of the template variable is used
    as label.
    
    Use the same approach in grafanalib, set the default value to None.
    
    The attribute needed to be moved down because of the following attrs exception:
      ValueError: No mandatory attributes allowed after an attribute with a default
      value or factory.  Attribute in question: Attribute(name='name',
      [..]

--- grafanalib/core.py
+++ grafanalib/core.py
@@ -515,9 +515,9 @@ class Template(object):
 
     default = attr.ib()
     dataSource = attr.ib()
-    label = attr.ib()
     name = attr.ib()
     query = attr.ib()
+    label = attr.ib(default=None)
     allValue = attr.ib(default=None)
     includeAll = attr.ib(
         default=False,
