9d4116b913c0e7cb55309031db6503521b116af9
Duncan Martin Walker <Duncan.Walker02@bbc.co.uk>
2020-04-06 16:51:39 +0100

    whitespace fix?

--- grafanalib/core.py
+++ grafanalib/core.py
@@ -2111,8 +2111,7 @@ class StatusmapColor(object):
 
 @attr.s
 class Statusmap(object):
-    """
-    Generates json structure for the flant-statusmap-panel visualisation plugin (https://grafana.com/grafana/plugins/flant-statusmap-panel/).
+    """Generates json structure for the flant-statusmap-panel visualisation plugin (https://grafana.com/grafana/plugins/flant-statusmap-panel/).
 
     :param alert: List of alerts to apply to the panel
     :param cards: A statusmap card object: keys: "cardRound", "cardMinWidth", "cardHSpacing", "cardVSpacing"
