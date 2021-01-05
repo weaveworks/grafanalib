3a6d5f233f2520040088520c1ba4b42ceff9d7ad
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2021-01-05 16:16:36 +0000

    Add Cloudwatch Target

---
+++ grafanalib/cloudwatch.py
@@ -0,0 +1,57 @@
+"""Helpers to create Cloudwatch-specific Grafana queries."""
+
+import attr
+
+from attr.validators import instance_of
+
+
+@attr.s
+class CloudwatchTarget(object):
+    """
+    Generates Cloudwatch target JSON structure.
+
+    Grafana docs on using Cloudwatch:
+    https://grafana.com/docs/features/datasources/cloudwatch/
+    AWS docs on Cloudwatch metrics:
+    https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/aws-services-cloudwatch-metrics.html
+    https://grafana.com/docs/grafana/latest/datasources/cloudwatch/#metric-math-expressions
+
+    :param alias: legend alias
+    :param dimensions: Cloudwatch dimensions dict
+    :param expression: Cloudwatch Metric math expressions
+    :param id: unique id
+    :param matchExact: Only show metrics that exactly match all defined dimension names.
+    :param metricName: Cloudwatch metric name
+    :param namespace: Cloudwatch namespace
+    :param period: Cloudwatch data period
+    :param refId: target reference id
+    :param region: Cloudwatch region
+    :param statistics: Cloudwatch mathematic statistic
+    """
+    alias = attr.ib(default="")
+    dimensions = attr.ib(default={}, validator=instance_of(dict))
+    expression = attr.ib(default="")
+    id = attr.ib(default="")
+    matchExact = attr.ib(default=True, validator=instance_of(bool))
+    metricName = attr.ib(default="")
+    namespace = attr.ib(default="")
+    period = attr.ib(default="")
+    refId = attr.ib(default="")
+    region = attr.ib(default="default")
+    statistics = attr.ib(default=["Average"], validator=instance_of(list))
+
+    def to_json_data(self):
+
+        return {
+            "alias": self.alias,
+            "dimensions": self.dimensions,
+            "expression": self.expression,
+            "id": self.id,
+            "matchExact": self.matchExact,
+            "metricName": self.metricName,
+            "namespace": self.namespace,
+            "period": self.period,
+            "refId": self.refId,
+            "region": self.region,
+            "statistics": self.statistics
+        }
---
+++ grafanalib/tests/test_cloudwatch.py
@@ -0,0 +1,30 @@
+"""Tests for Cloudwatch Datasource"""
+
+import grafanalib.core as G
+import grafanalib.cloudwatch as C
+from grafanalib import _gen
+
+import sys
+if sys.version_info[0] < 3:
+    from io import BytesIO as StringIO
+else:
+    from io import StringIO
+
+
+def test_serialization_cloudwatch_target():
+    """Serializing a graph doesn't explode."""
+    graph = G.Graph(
+        title="Lambda Duration",
+        dataSource="Cloudwatch data source",
+        targets=[
+            C.CloudwatchTarget(),
+        ],
+        id=1,
+        yAxes=G.YAxes(
+            G.YAxis(format=G.SHORT_FORMAT, label="ms"),
+            G.YAxis(format=G.SHORT_FORMAT),
+        ),
+    )
+    stream = StringIO()
+    _gen.write_dashboard(graph, stream)
+    assert stream.getvalue() != ''
