fe16e77db5293152b3aa0e2b426bdd5a43c0e9c8
Robert Cowham <rcowham@perforce.com>
2021-04-13 14:31:10 +0100

    Fix flake8 warnings

--- grafanalib/tests/examples/table-example.dashboard.py
+++ grafanalib/tests/examples/table-example.dashboard.py
@@ -39,39 +39,37 @@ metrics = [
               'sum(monitor_by_cmd{serverid="$serverid"})']},
     {'title': 'Monitor Processes (by user)',
      'expr': ['monitor_by_user{serverid="$serverid"}',
-             'sum(monitor_by_user{serverid="$serverid"})']},
+              'sum(monitor_by_user{serverid="$serverid"})']},
 ]
 
 
 class CreateDashboard():
-    """See module doc string for details"""
+    "See module doc string for details"
 
     def __init__(self, *args, **kwargs):
         self.parse_args(__doc__, args)
 
     def parse_args(self, doc, args):
-        """Common parsing and setting up of args"""
+        "Common parsing and setting up of args"
         desc = textwrap.dedent(doc)
         parser = argparse.ArgumentParser(
             formatter_class=argparse.RawDescriptionHelpFormatter,
-            description=desc
-        )
-        parser.add_argument('-t', '--title', default=DEFAULT_TITLE, help="Dashboard title. Default: " + DEFAULT_TITLE)
+            description=desc)
+        parser.add_argument('-t', '--title', default=DEFAULT_TITLE,
+                            help="Dashboard title. Default: " + DEFAULT_TITLE)
         self.options = parser.parse_args(args=args)
 
     def run(self):
-        templateList = [G.Template(
-                    default="",
-                    dataSource="default",
-                    name="serverid",
-                    label="ServerID",
-                    query="label_values(serverid)")]
+        templateList = [G.Template(default="",
+                                   dataSource="default",
+                                   name="serverid",
+                                   label="ServerID",
+                                   query="label_values(serverid)")]
 
-        dashboard = G.Dashboard(
-            title=self.options.title,
-            templating=G.Templating(list=templateList)
-        )
+        dashboard = G.Dashboard(title=self.options.title,
+                                templating=G.Templating(list=templateList))
 
+        # Simple table processing - could be enhanced to use GridPos etc.
         for metric in metrics:
             if 'section' in metric:
                 dashboard.rows.append(G.Row(title=metric['section'], showTitle=True))
@@ -83,15 +81,14 @@ class CreateDashboard():
                             dataSource='default',
                             maxDataPoints=1000,
                             legend=G.Legend(show=True, alignAsTable=True,
-                                        min=True, max=True, avg=True, current=True, total=True,
-                                        sort='max', sortDesc=True),
-                            yAxes=G.single_y_axis(),
-                        )
-            refId = 'A'
+                                            min=True, max=True, avg=True, current=True, total=True,
+                                            sort='max', sortDesc=True),
+                            yAxes=G.single_y_axis())
+            ref_id = 'A'
             for texp in metric['expr']:
                 graph.targets.append(G.Target(expr=texp,
-                                              refId=refId))
-                refId = chr(ord(refId) + 1)
+                                              refId=ref_id))
+                ref_id = chr(ord(ref_id) + 1)
             dashboard.rows[-1].panels.append(graph)
 
         # Auto-number panels - returns new dashboard
@@ -103,6 +100,7 @@ class CreateDashboard():
         "dashboard": %s
         }
         """ % s.getvalue())
+
 
 if __name__ == '__main__':
     """ Main Program"""
