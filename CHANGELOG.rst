c27394cbef7ef5df32b9df3e4470ed62f8875e4c
milkpirate <milkpirate@users.noreply.github.com>
2021-10-04 11:00:40 +0200

    Discrete options (#407)
    * ignore venv and .idea
    
    * restructure and add supporting classes
    
    * update discrete class
    
    * add tests
    
    * correct chglg https://github.com/weaveworks/grafanalib/pull/399/files#diff-e183577edc3e65361ecd131b1d852e4b241b30f61edd379a745317aa49502339R490
    
    * correct double def of ValueMap
    
    * add panel URL
    
    Co-authored-by: JamesGibo <james.gibson@bbc.co.uk>
    
    * correct indentation
    
    * add note to docs
    
    * add textSize
    
    * add textSizeTime
    
    Co-authored-by: Paul Schroeder <paul.schroeder@localhost>
    Co-authored-by: JamesGibo <james.gibson@bbc.co.uk>

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -14,7 +14,7 @@ Changes
 
 * Refine expectations of is_color_code
 * Deprecated StatMapping, StatValueMapping & StatRangeMapping
-* Change YAxis min value default from None to 0
+* Change YAxis min value default from 0 to None
 
 0.5.14 (2021-09-14)
 ==================
--- grafanalib/core.py
+++ grafanalib/core.py
@@ -1477,46 +1477,24 @@ class Graph(Panel):
 
 
 @attr.s
-class DiscreteColorMappingItem(object):
+class ValueMap(object):
     """
-    Generates json structure for the value mapping item for the StatValueMappings class:
+    Generates json structure for a value mapping item.
 
-    :param text: String to color
-    :param color: To color the text with
+    :param op: comparison operator
+    :param value: value to map to text
+    :param text: text to map the value to
     """
-
-    text = attr.ib(validator=instance_of(str))
-    color = attr.ib(default=GREY1, validator=instance_of((str, RGBA)))
+    text = attr.ib()
+    value = attr.ib()
+    op = attr.ib(default='=')
 
     def to_json_data(self):
         return {
-            "color": self.color,
-            "text": self.text,
+            'op': self.op,
+            'text': self.text,
+            'value': self.value,
         }
-
-
-@attr.s
-class Discrete(Panel):
-    """
-    Generates Discrete panel json structure.
-
-    :param colorMaps: List of DiscreteColorMappingItem, to color values.
-    """
-
-    colorMapsItems = attr.ib(
-        default=[],
-        validator=attr.validators.deep_iterable(
-            member_validator=attr.validators.instance_of(DiscreteColorMappingItem),
-            iterable_validator=attr.validators.instance_of(list),
-        ),
-    )
-
-    def to_json_data(self):
-        graphObject = {
-            'colorMaps': self.colorMapsItems,
-            'type': DISCRETE_TYPE,
-        }
-        return self.panel_json(graphObject)
 
 
 @attr.s
@@ -1542,16 +1520,21 @@ class SparkLine(object):
 
 
 @attr.s
-class ValueMap(object):
-    op = attr.ib()
-    text = attr.ib()
-    value = attr.ib()
+class Gauge(object):
+
+    minValue = attr.ib(default=0, validator=instance_of(int))
+    maxValue = attr.ib(default=100, validator=instance_of(int))
+    show = attr.ib(default=False, validator=instance_of(bool))
+    thresholdLabels = attr.ib(default=False, validator=instance_of(bool))
+    thresholdMarkers = attr.ib(default=True, validator=instance_of(bool))
 
     def to_json_data(self):
         return {
-            'op': self.op,
-            'text': self.text,
-            'value': self.value,
+            'maxValue': self.maxValue,
+            'minValue': self.minValue,
+            'show': self.show,
+            'thresholdLabels': self.thresholdLabels,
+            'thresholdMarkers': self.thresholdMarkers,
         }
 
 
@@ -1570,22 +1553,176 @@ class RangeMap(object):
 
 
 @attr.s
-class Gauge(object):
+class DiscreteColorMappingItem(object):
+    """
+    Generates json structure for the value mapping item for the StatValueMappings class:
 
-    minValue = attr.ib(default=0, validator=instance_of(int))
-    maxValue = attr.ib(default=100, validator=instance_of(int))
-    show = attr.ib(default=False, validator=instance_of(bool))
-    thresholdLabels = attr.ib(default=False, validator=instance_of(bool))
-    thresholdMarkers = attr.ib(default=True, validator=instance_of(bool))
+    :param text: String to color
+    :param color: To color the text with
+    """
+
+    text = attr.ib(validator=instance_of(str))
+    color = attr.ib(default=GREY1, validator=instance_of((str, RGBA)))
 
     def to_json_data(self):
         return {
-            'maxValue': self.maxValue,
-            'minValue': self.minValue,
-            'show': self.show,
-            'thresholdLabels': self.thresholdLabels,
-            'thresholdMarkers': self.thresholdMarkers,
+            "color": self.color,
+            "text": self.text,
         }
+
+
+@attr.s
+class Discrete(Panel):
+    """
+    Generates Discrete panel json structure.
+    https://grafana.com/grafana/plugins/natel-discrete-panel/
+
+    :param colorMaps: list of DiscreteColorMappingItem, to color values
+        (note these apply **after** value mappings)
+    :param backgroundColor: dito
+    :param lineColor: Separator line color between rows
+    :param metricNameColor: dito
+    :param timeTextColor: dito
+    :param valueTextColor: dito
+
+    :param decimals: number of decimals to display
+    :param rowHeight: dito
+
+    :param units: defines value units
+    :param legendSortBy: time (desc: '-ms', asc: 'ms), count (desc: '-count', asc: 'count')
+
+    :param highlightOnMouseover: whether to highlight the state of hovered time falls in.
+    :param showLegend: dito
+    :param showLegendPercent: whether to show percentage of time spent in each state/value
+    :param showLegendNames:
+    :param showLegendValues: whether to values in legend
+    :param legendPercentDecimals: number of decimals for legend
+    :param showTimeAxis: dito
+    :param use12HourClock: dito
+    :param writeMetricNames: dito
+    :param writeLastValue: dito
+    :param writeAllValues: whether to show all values
+
+    :param showDistinctCount: whether to show distinct values count
+    :param showLegendCounts: whether to show value occurrence count
+    :param showLegendTime: whether to show of each state
+    :param showTransitionCount: whether to show transition count
+
+    :param colorMaps: list of DiscreteColorMappingItem
+    :param rangeMaps: list of RangeMap
+    :param valueMaps: list of ValueMap
+    """
+
+    backgroundColor = attr.ib(
+        default=RGBA(128, 128, 128, 0.1),
+        validator=instance_of((RGBA, RGB, str))
+    )
+    lineColor = attr.ib(
+        default=RGBA(0, 0, 0, 0.1),
+        validator=instance_of((RGBA, RGB, str))
+    )
+    metricNameColor = attr.ib(
+        default="#000000",
+        validator=instance_of((RGBA, RGB, str))
+    )
+    timeTextColor = attr.ib(
+        default="#d8d9da",
+        validator=instance_of((RGBA, RGB, str))
+    )
+    valueTextColor = attr.ib(
+        default="#000000",
+        validator=instance_of((RGBA, RGB, str))
+    )
+
+    decimals = attr.ib(default=0, validator=instance_of(int))
+    legendPercentDecimals = attr.ib(default=0, validator=instance_of(int))
+    rowHeight = attr.ib(default=50, validator=instance_of(int))
+    textSize = attr.ib(default=24, validator=instance_of(int))
+
+    textSizeTime = attr.ib(default=12, validator=instance_of(int))
+    units = attr.ib(default="none", validator=instance_of(str))
+    legendSortBy = attr.ib(
+        default="-ms",
+        validator=in_(['-ms', 'ms', '-count', 'count'])
+    )
+
+    highlightOnMouseover = attr.ib(default=True, validator=instance_of(bool))
+    showLegend = attr.ib(default=True, validator=instance_of(bool))
+    showLegendPercent = attr.ib(default=True, validator=instance_of(bool))
+    showLegendNames = attr.ib(default=True, validator=instance_of(bool))
+    showLegendValues = attr.ib(default=True, validator=instance_of(bool))
+    showTimeAxis = attr.ib(default=True, validator=instance_of(bool))
+    use12HourClock = attr.ib(default=False, validator=instance_of(bool))
+    writeMetricNames = attr.ib(default=False, validator=instance_of(bool))
+    writeLastValue = attr.ib(default=True, validator=instance_of(bool))
+    writeAllValues = attr.ib(default=False, validator=instance_of(bool))
+
+    showDistinctCount = attr.ib(default=None)
+    showLegendCounts = attr.ib(default=None)
+    showLegendTime = attr.ib(default=None)
+    showTransitionCount = attr.ib(default=None)
+
+    colorMaps = attr.ib(
+        default=[],
+        validator=attr.validators.deep_iterable(
+            member_validator=instance_of(DiscreteColorMappingItem),
+            iterable_validator=instance_of(list),
+        ),
+    )
+    rangeMaps = attr.ib(
+        default=[],
+        validator=attr.validators.deep_iterable(
+            member_validator=instance_of(RangeMap),
+            iterable_validator=instance_of(list),
+        ),
+    )
+    valueMaps = attr.ib(
+        default=[],
+        validator=attr.validators.deep_iterable(
+            member_validator=instance_of(ValueMap),
+            iterable_validator=instance_of(list),
+        ),
+    )
+
+    def to_json_data(self):
+        graphObject = {
+            'type': DISCRETE_TYPE,
+
+            'backgroundColor': self.backgroundColor,
+            'lineColor': self.lineColor,
+            'metricNameColor': self.metricNameColor,
+            'timeTextColor': self.timeTextColor,
+            'valueTextColor': self.valueTextColor,
+            'legendPercentDecimals': self.legendPercentDecimals,
+            'decimals': self.decimals,
+            'rowHeight': self.rowHeight,
+            'textSize': self.textSize,
+            'textSizeTime': self.textSizeTime,
+
+            'units': self.units,
+            'legendSortBy': self.legendSortBy,
+
+            'highlightOnMouseover': self.highlightOnMouseover,
+            'showLegend': self.showLegend,
+            'showLegendPercent': self.showLegendPercent,
+            'showLegendNames': self.showLegendNames,
+            'showLegendValues': self.showLegendValues,
+            'showTimeAxis': self.showTimeAxis,
+            'use12HourClock': self.use12HourClock,
+            'writeMetricNames': self.writeMetricNames,
+            'writeLastValue': self.writeLastValue,
+            'writeAllValues': self.writeAllValues,
+
+            'showDistinctCount': self.showDistinctCount,
+            'showLegendCounts': self.showLegendCounts,
+            'showLegendTime': self.showLegendTime,
+            'showTransitionCount': self.showTransitionCount,
+
+            'colorMaps': self.colorMaps,
+            'rangeMaps': self.rangeMaps,
+            'valueMaps': self.valueMaps,
+        }
+        return self.panel_json(graphObject)
 
 
 @attr.s
