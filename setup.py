5884288246ab77d9cb92315e9352cb4a624c3925
rjulius23 <gyula.halmos@gmail.com>
2021-11-17 14:52:02 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.6.0',
+    version='0.6.1',
     description='Library for building Grafana dashboards',
     long_description=open(README).read(),
     url='https://github.com/weaveworks/grafanalib',
