329f71713769f826824e0fc19efe1f46f9c27d0b
James Gibson <22477854+JamesGibo@users.noreply.github.com>
2024-01-12 10:32:14 +0000

    chore: prep for 7.0.1 release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
-    version='0.7.0',
+    version='0.7.1',
     description='Library for building Grafana dashboards',
     long_description=open(README).read(),
     url='https://github.com/weaveworks/grafanalib',
